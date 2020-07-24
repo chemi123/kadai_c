6f0eb8a9570af62075b77e97feda235bf219378a
Yuichi Tanimoto <>
2020-07-24 17:41:53 +0900

    add test for collectMap2

--- c_test.go
+++ c_test.go
@@ -37,3 +37,38 @@ func Test_collectMap(t *testing.T) {
 		}
 	}
 }
+
+func Test_collectMap2(t *testing.T) {
+	testCases := []struct {
+		name      string
+		schedules [][]int
+		maxDay    int
+		wantDay   int
+	}{
+		{
+			name:      "succeed to collect all the pieces of map",
+			schedules: [][]int{[]int{1, 6}, []int{2, 3}, []int{1, 2}, []int{3, 4, 5}},
+			maxDay:    6,
+			wantDay:   3,
+		},
+		{
+			name:      "fail to collect all the pieces of map",
+			schedules: [][]int{[]int{2}, []int{5}},
+			maxDay:    5,
+			wantDay:   -1,
+		},
+		{
+			name:      "one succesor",
+			schedules: [][]int{[]int{2, 5, 10, 30}},
+			maxDay:    30,
+			wantDay:   0,
+		},
+	}
+	for _, tc := range testCases {
+		day := collectMap2(tc.schedules, tc.maxDay)
+		if day != tc.wantDay {
+			t.Fatalf("test fail. want: %v, got: %v\n", tc.wantDay, day)
+		}
+	}
+
+}
