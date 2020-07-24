ce45d3ac422bbad68280668a930d725feabeb133
Yuichi Tanimoto <>
2020-07-24 14:10:28 +0900

    add create_dataset.py

---
+++ create_dataset.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 create_dataset.py ${dataset_num}")
+        exit(1)
+    
+    if not sys.argv[1].isdecimal():
+        print("input number for dataset_num")
+        exit(1)
+    
