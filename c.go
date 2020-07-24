358a7b92d0bc21146fbe1ea3fc7510cbaa6d0cb9
Yuichi Tanimoto <>
2020-07-24 10:29:05 +0900

    add maxDay

--- c.go
+++ c.go
@@ -9,12 +9,19 @@ import (
 )
 
 /*
-  方針: 冒険者ごとのスケジュールのリスト(shedules [][]int)から日付ごとに集まれる冒険者一覧のリストを作る。
-	   そこから集まった冒険者が地図を集まった分だけ持つようにし、次に集まった時にそれをさらなる冒険者に渡すというのを繰り返す。
+  方針: 継承者ごとのスケジュールのリスト(shedules [][]int)から日付ごとに集まれる継承者一覧のリストを作る。
+	   そこから集まった継承者が地図を集まった分だけ持つようにし、次に集まった時にそれをさらなる継承者に渡すというのを繰り返す。
 	   直感的な実装になりそう。ここでは試さないが、グラフを使った探索もできそう？
 */
 func collectMap(schedules [][]int) int {
 	return -1
+}
+
+func max(num1, num2 int) int {
+	if num1 > num2 {
+		return num1
+	}
+	return num2
 }
 
 func main() {
@@ -28,16 +35,19 @@ func main() {
 		n, _ := strconv.Atoi(scanner.Text())
 		fmt.Println(n)
 		schedules := make([][]int, 0, n)
+		maxDay := 0
 		for j := 0; j < n; j++ {
 			scanner.Scan()
 			daysStr := strings.Split(scanner.Text(), " ")
 			days := make([]int, 0, len(daysStr))
 			for _, dayStr := range daysStr {
 				day, _ := strconv.Atoi(dayStr)
+				maxDay = max(maxDay, day)
 				days = append(days, day)
 			}
 			schedules = append(schedules, days)
 		}
 		fmt.Println(schedules)
+		fmt.Println(maxDay)
 	}
 }
