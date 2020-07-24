31353c47af0b00e291aecbf41daa4b435ec29cdc
Yuichi Tanimoto <>
2020-07-24 11:40:45 +0900

    add test cases

--- c_test.go
+++ c_test.go
@@ -10,11 +10,24 @@ func Test_collectMap(t *testing.T) {
 		schedules [][]int
 		maxDay    int
 		wantDay   int
-	}{}
+	}{
+		{
+			name:      "succeed to collect all the pieces of map",
+			schedules: [][]int{[]int{1, 6}, []int{2, 3}, []int{1, 2}, []int{3, 4, 5}},
+			maxDay:    6,
+			wantDay:   3,
+		},
+		{
+			name:      "fail to collect all the pieces of map",
+			schedules: [][]int{[]int{2}, []int{5}},
+			maxDay:    5,
+			wantDay:   -1,
+		},
+	}
 	for _, tc := range testCases {
 		day := collectMap(tc.schedules, tc.maxDay)
 		if day != tc.wantDay {
-			t.Fatalf("")
+			t.Fatalf("test fail. want: %v, got: %v\n", tc.wantDay, day)
 		}
 	}
 }
